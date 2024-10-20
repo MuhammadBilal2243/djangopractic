f3e17d50e9a73b9b5c46ac7293d9f99388492580
MuhammadBilal2243 <bilal0302599@gmail.com>
2024-10-20 12:20:06 -0700

    to add some more concepts to my project like model migration and makemigrarions commands

--- ecom/shop/urls.py
+++ ecom/shop/urls.py
@@ -3,4 +3,10 @@ from . import  views
 urlpatterns = [
 
     path("",views.index,name="shop"),
+    path("contect/",views.contect,name="contect"),
+    path("about/",views.about,name="about"),
+    path("search/",views.search,name="search"),
+    path("prodview",views.prodview,name="prodview"),
+    path("checkout",views.checkout,name="checkout"),
+
 ]
--- ecom/shop/views.py
+++ ecom/shop/views.py
@@ -2,3 +2,13 @@ from django.shortcuts import render
 from  django.http import  HttpResponse
 def index(request):
     return  render(request,'shop/index.html')
+def contect(request):
+    return  HttpResponse("this my contect")
+def about(request):
+    return  HttpResponse("this my about")
+def search(request):
+    return  HttpResponse("this my search")
+def prodview(request):
+    return  HttpResponse("this my prodview")
+def checkout(request):
+    return  HttpResponse("this my checkout")
