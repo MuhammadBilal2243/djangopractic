e8aeccfda0b6686d9b09e7a28dffbfa6e1380182
MuhammadBilal2243 <bilal0302599@gmail.com>
2024-10-22 03:17:29 -0700

    adding more feature to project

--- ecom/shop/models.py
+++ ecom/shop/models.py
@@ -3,8 +3,15 @@ from django.db import models
 # Create your models here.
 class product(models.Model):
      product_id = models.AutoField
-     product_name = models.CharField(max_length=50)
-     desc = models.CharField(max_length=500)
+     product_name = models.CharField(max_length=50,default='')
+     category =models.CharField(max_length=50 ,default='')
+     sub_category =models.CharField(max_length=50 ,default='')
+     desc = models.CharField(max_length=500,default='')
+     price=models.IntegerField(default=0)
      pub_date = models.DateField()
+     image=models.ImageField(upload_to='shop/images' ,default='')
+
+     def __str__(self):
+          return  self.product_name
 
 
