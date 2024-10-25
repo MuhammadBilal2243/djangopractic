075486c50e7a2cfc003acaec9065596a8e76cfc5
MuhammadBilal2243 <bilal0302599@gmail.com>
2024-10-25 00:17:09 -0700

    adsasfa dffgs

--- ecom/shop/views.py
+++ ecom/shop/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from  django.http import  HttpResponse
+from .models import product
+from math import  ceil
 def index(request):
-    return  render(request,'shop/index.html')
+    products = product.objects.all()
+    print(products)
+    n=len(products)
+    no_slides=(n//4)+ceil((n/4)-(n//4))
+    parems={'product':products,'no_of_slides':no_slides,'range':range(1,no_slides)}
+
+    return  render(request,'shop/index.html',parems)
 def contect(request):
     return  HttpResponse("this my contect")
 def about(request):
